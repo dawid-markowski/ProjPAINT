36744ea06e260fbe904ad4a5c10cb7adcefac3bf
dawid-markowski <dawid.markowski.pl@gmail.com>
2025-01-22 22:04:41 +0100

    prosta wyszukiwarka

--- app/shop/routes.py
+++ app/shop/routes.py
@@ -7,17 +7,20 @@ from flask_login import login_required, current_user
 
 
 
-@bp.route('/parts')
+@bp.route('/parts', methods=['GET'])
 @login_required
 def parts():
-    parts = db.session.scalars(sa.select(Part)).all()
+    search_query = request.args.get('search', '')  # Pobieramy parametr "search" z URL
+    if search_query:
+        parts = db.session.scalars(sa.select(Part).where(sa.func.lower(Part.part_name).like(sa.func.lower(f'%{search_query}%')))).all()  # Wyszukiwanie po nazwie, podstawowe
+    else:
+        parts = db.session.scalars(sa.select(Part)).all()
     return render_template('shop/parts.html', title='Czesci Samochodowe', parts=parts)
 
 
 @bp.route('/parts/<part>', methods=['GET', 'POST'])
 @login_required
 def part(part):
-    print(part)
     query = sa.select(Part).where(Part.part_name == part)
     part= db.session.scalars(query).first()
     return render_template('shop/part.html',part=part)
