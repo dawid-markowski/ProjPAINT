14240573312249f295f798a0c6ebf2438657f214
dawid-markowski <dawid.markowski.pl@gmail.com>
2025-01-26 20:58:51 +0100

    konfiguracja pod dockera

--- stronka.py
+++ stronka.py
@@ -9,7 +9,11 @@ def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Part': Part, 'Comment': Comment}
 
 
+#do dockera
+#app = create_app()
+
+##Jesli uzywasz lokalnego z pythona
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=False)
 
